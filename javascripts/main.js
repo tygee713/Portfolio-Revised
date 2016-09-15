38bef5f7eae01b100dd388c57ee82251d15c47df
Tyler Geerdts <tgeerdts@Tylers-MacBook-Pro.local>
2016-09-15 13:14:08 -0400

    Fixed home height

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $('body').scrollspy({ target: "#myNav", offset: 50 });
+  var homeheight = $(window).height();
+  $('#home').css('height', homeheight - 50 + 'px');
 })
 
 $('nav a').on('click', function(e) {
@@ -11,20 +13,16 @@ $('nav a').on('click', function(e) {
   }, 500)
 })
 
-var num = $('#aboutSection').offset().top - 50; //number of pixels before fixing nav bar to top
-
 $(window).bind('scroll', function () {
+  var num = $('#aboutSection').offset().top - 50; //number of pixels before fixing nav bar to top
     if ($(window).scrollTop() >= num) {
         $('#myNav').removeClass('navbar-fixed-bottom');
         $('#myNav').addClass('navbar-fixed-top');
         $('#aboutSection').css('margin-top', '50px');
-        console.log($('#aboutSection').offset().top);
-
     } 
     else {
          $('#myNav').removeClass('navbar-fixed-top');
          $('#myNav').addClass('navbar-fixed-bottom');
          $('#aboutSection').css('margin-top', '0px');
-         console.log($('#aboutSection').offset().top);
     }
 });
